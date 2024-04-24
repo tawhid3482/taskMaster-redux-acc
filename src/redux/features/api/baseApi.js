e3db618a410a80fdf9d9444b3c54d8f85088cfdb
saikat <tawhidulislam3482@gmail.com>
2024-04-24 16:11:03 +0600

    base api concept

---
+++ src/redux/features/api/baseApi.js
@@ -0,0 +1,15 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://jsonplaceholder.typicode.com",
+  }),
+  endpoints: (builder) => ({
+    getPosts: builder.query({
+      query: () => "/posts",
+    }),
+  }),
+});
+export const { useGetPostsQuery } = baseApi;
+export default baseApi;
--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import taskSlice from "./features/task/taskSlice";
 import userSlice from "./users/userSlice";
+import baseApi from "./features/api/baseApi";
 // import logger from './middlewares/logger'
 const store = configureStore({
   reducer: {
     taskSlice: taskSlice,
-    userSlice : userSlice,
+    userSlice: userSlice,
+    [baseApi.reducerPath]: baseApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(baseApi.middleware),
   //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 export default store;
